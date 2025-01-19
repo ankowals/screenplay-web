498afb97998faf60f3ade8da489b4eb4e5f328a3
cc cc <31848642+ankowals@users.noreply.github.com>
2025-01-19 16:28:09 +0100

    refactoring

--- src/test/java/framework/screenplay/helpers/InParallel.java
+++ src/test/java/framework/screenplay/helpers/InParallel.java
@@ -1,15 +1,29 @@
 package framework.screenplay.helpers;
 
+import framework.screenplay.Interaction;
+import framework.screenplay.actor.Actor;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.Supplier;
+import org.apache.commons.lang3.function.FailableCallable;
 import org.apache.commons.lang3.function.FailableRunnable;
-import org.apache.commons.lang3.stream.Streams;
 
 public class InParallel {
 
+  private final Actor[] actors;
+
+  private InParallel(Actor[] actors) {
+    this.actors = actors;
+  }
+
+  public static InParallel actors(Actor... actors) {
+    return new InParallel(actors);
+  }
+
   /**
-   * InParallel.perform( () -> actor1.attemptsTo(Find.productDetails("Dress")), () ->
-   * actor2.attemptsTo(Find.productDetails("Other Dress")));
+   * InParallel.perform( () -> actor1.attemptsTo(Buy.product("Dress")), () ->
+   * actor2.attemptsTo(Buy.product("Other Dress")));
    */
   public static void perform(FailableRunnable<?>... failableRunnables) {
     InParallel.perform(8, failableRunnables);
@@ -17,12 +31,92 @@ public class InParallel {
 
   public static void perform(int parallelism, FailableRunnable<?>... failableRunnables) {
     try (ForkJoinPool forkJoinPool = new ForkJoinPool(parallelism)) {
-      forkJoinPool
-          .submit(
+      CompletableFuture<?>[] completableFutures =
+          Arrays.stream(failableRunnables)
+              .map(failableRunnable -> InParallel.toFuture(failableRunnable, forkJoinPool))
+              .toList()
+              .toArray(new CompletableFuture[0]);
+
+      CompletableFuture.allOf(completableFutures).join();
+
+      /*
+      CompletableFuture.runAsync(
               () ->
                   Streams.failableStream(Arrays.stream(failableRunnables).parallel())
-                      .forEach(FailableRunnable::run))
+                      .forEach(FailableRunnable::run),
+              forkJoinPool)
           .join();
+       */
     }
+  }
+
+  @SafeVarargs
+  public static <T> List<T> performAndGet(FailableCallable<T, Exception>... callables) {
+    return InParallel.performAndGet(8, callables);
+  }
+
+  @SafeVarargs
+  public static <T> List<T> performAndGet(
+      int parallelism, FailableCallable<T, Exception>... callables) {
+    try (ForkJoinPool forkJoinPool = new ForkJoinPool(parallelism)) {
+      List<CompletableFuture<T>> completableFutures =
+          Arrays.stream(callables)
+              .map(callable -> InParallel.toFuture(callable, forkJoinPool))
+              .toList();
+
+      CompletableFuture<List<T>> allOfFuture =
+          CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]))
+              .thenApply(t -> completableFutures.stream().map(CompletableFuture::join).toList());
+
+      return allOfFuture.join();
+    }
+  }
+
+  /** InParallel.actors(actor1, actor2).eachAttemptsTo(() -> Buy.product("Dress")); */
+  @SafeVarargs
+  public final void eachAttemptsTo(Supplier<Interaction>... suppliers) {
+    this.eachAttemptsTo(8, suppliers);
+  }
+
+  @SafeVarargs
+  public final void eachAttemptsTo(int parallelism, Supplier<Interaction>... suppliers) {
+    List<FailableRunnable<Exception>> failableRunnables =
+        Arrays.stream(suppliers)
+            .flatMap(
+                supplier ->
+                    Arrays.stream(this.actors)
+                        .map(
+                            actor ->
+                                (FailableRunnable<Exception>)
+                                    () -> actor.attemptsTo(supplier.get())))
+            .toList();
+
+    InParallel.perform(parallelism, failableRunnables.toArray(new FailableRunnable[] {}));
+  }
+
+  private static CompletableFuture<Void> toFuture(
+      FailableRunnable<?> failableRunnable, ForkJoinPool forkJoinPool) {
+    return CompletableFuture.runAsync(
+        () -> {
+          try {
+            failableRunnable.run();
+          } catch (Throwable e) {
+            throw new RuntimeException(e);
+          }
+        },
+        forkJoinPool);
+  }
+
+  private static <T> CompletableFuture<T> toFuture(
+      FailableCallable<T, Exception> failableCallable, ForkJoinPool forkJoinPool) {
+    return CompletableFuture.supplyAsync(
+        () -> {
+          try {
+            return failableCallable.call();
+          } catch (Exception e) {
+            throw new RuntimeException(e);
+          }
+        },
+        forkJoinPool);
   }
 }
--- src/test/java/tests/screenplay/ScreenplayTasksTest.java
+++ src/test/java/tests/screenplay/ScreenplayTasksTest.java
@@ -7,8 +7,13 @@ import framework.screenplay.abilities.memory.TheValue;
 import framework.screenplay.actor.Actor;
 import framework.screenplay.helpers.InParallel;
 import framework.screenplay.helpers.See;
+import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class ScreenplayTasksTest extends ScreenplayTestBase {
 
@@ -68,15 +73,68 @@ class ScreenplayTasksTest extends ScreenplayTestBase {
         .isNotEqualTo(TheValue.rememberedAs("thread3", String.class).answeredBy(this.actor));
   }
 
-  static class Devices {
-    static Device virtualDevice() {
-      return new Device(RandomStringUtils.insecure().nextAlphabetic(8));
+  @Test
+  @Order(3)
+  void shouldRunParallelInteractionsByEachActor() throws Exception {
+    Actor actor1 = Actors.withMemory();
+    Actor actor2 = Actors.withMemory();
+    Actor actor3 = Actors.withMemory();
+    Actor actor4 = Actors.withMemory();
+    Actor actor5 = Actors.withMemory();
+    Actor actor6 = Actors.withMemory();
+    Actor actor7 = Actors.withMemory();
+    Actor actor8 = Actors.withMemory();
+    Actor actor9 = Actors.withMemory();
+
+    InParallel.actors(actor1, actor2, actor3, actor4, actor5, actor6, actor7, actor8, actor9)
+        .eachAttemptsTo(() -> RememberThat.valueOf("thread").is(Thread.currentThread().getName()));
+
+    actor1.should(See.that(TheValue.rememberedAs("thread", String.class))).isNotNull();
+    actor3.should(See.that(TheValue.rememberedAs("thread", String.class))).isNotNull();
+
+    actor1
+        .should(See.that(TheValue.rememberedAs("thread", String.class)))
+        .isNotEqualTo(TheValue.rememberedAs("thread", String.class).answeredBy(actor3));
+  }
+
+  @Test
+  @Order(4)
+  void shouldRunParallelQuestions() {
+    Actor actor1 = Actors.withMemory();
+    Actor actor2 = Actors.withMemory();
+    Actor actor3 = Actors.withMemory();
+
+    InParallel.actors(actor1, actor2, actor3)
+        .eachAttemptsTo(() -> RememberThat.valueOf("thread").is(Thread.currentThread().getName()));
+
+    List<String> threads =
+        InParallel.performAndGet(
+            () -> TheValue.rememberedAs("thread", String.class).answeredBy(actor1),
+            () -> TheValue.rememberedAs("thread", String.class).answeredBy(actor2),
+            () -> TheValue.rememberedAs("thread", String.class).answeredBy(actor3));
+
+    Assertions.assertThat(threads).isNotEmpty().hasSize(3);
+    Assertions.assertThat(threads.getFirst()).isNotEqualTo(threads.getLast());
+  }
+
+  static class Actors {
+    static Actor withMemory() {
+      Actor anActor = new Actor();
+      anActor.can(RememberThings.with(new Memory()));
+
+      return anActor;
     }
   }
 
   static class Customers {
     static Customer tequila() {
       return new Customer("Tequila123");
+    }
+  }
+
+  static class Devices {
+    static Device virtualDevice() {
+      return new Device(RandomStringUtils.insecure().nextAlphabetic(8));
     }
   }
 

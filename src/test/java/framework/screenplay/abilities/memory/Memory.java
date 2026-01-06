5172445f4bc9135a3334a2a7c2fd971b008364f5
ankowals <31848642+ankowals@users.noreply.github.com>
2026-01-06 10:24:05 +0100

    refactoring

--- src/test/java/framework/screenplay/abilities/memory/Memory.java
+++ src/test/java/framework/screenplay/abilities/memory/Memory.java
@@ -8,32 +8,26 @@ import java.util.concurrent.ConcurrentHashMap;
  * create your own wrapper type and use in place of collection type.
  */
 public class Memory {
-  private final Map<Key<?>, Object> memory;
+  private final Map<Key<?>, Object> cache;
 
   public Memory() {
-    this.memory = new ConcurrentHashMap<>();
+    this.cache = new ConcurrentHashMap<>();
   }
 
-  public <T> void remember(String name, T value) {
-    this.memory.put(new Key<>(name, value.getClass()), value);
+  <T> void remember(String name, T value) {
+    this.cache.put(new Key<>(name, value.getClass()), value);
   }
 
-  public <T> T recall(String name, Class<T> type) {
-    Object object = this.memory.get(new Key<>(name, type));
-
-    if (object == null) {
-      throw new NoObjectToRecallException(name);
-    }
-
-    return type.cast(object);
+  <T> T recall(String name, Class<T> type) {
+    return type.cast(this.cache.get(new Key<>(name, type)));
   }
 
-  public <T> void forget(String name, Class<T> type) {
-    this.memory.remove(new Key<>(name, type));
+  <T> void forget(String name, Class<T> type) {
+    this.cache.remove(new Key<>(name, type));
   }
 
-  public void clear() {
-    this.memory.clear();
+  void clear() {
+    this.cache.clear();
   }
 
   record Key<T>(String name, Class<T> type) {}
--- src/test/java/framework/screenplay/abilities/memory/TheRemembered.java
+++ src/test/java/framework/screenplay/abilities/memory/TheRemembered.java
@@ -5,6 +5,14 @@ import framework.screenplay.actor.use.UseAbility;
 
 public class TheRemembered {
   public static <T> Question<T> valueOf(String key, Class<T> type) {
-    return actor -> UseAbility.of(actor).to(RememberThings.class).memory().recall(key, type);
+    return actor -> {
+      T value = UseAbility.of(actor).to(RememberThings.class).memory().recall(key, type);
+
+      if (value == null) {
+        throw new NoObjectToRecallException(key);
+      }
+
+      return value;
+    };
   }
 }
